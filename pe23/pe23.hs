e225f20a60347e5aef4f84106cd92d4722132d1f
SeeMemes <provotorov-02@mail.ru>
2022-10-24 06:23:50 +0300

    added v23

---
+++ pe23/pe23.hs
@@ -0,0 +1,29 @@
+import Data.List
+import Data.Array.ST
+import Control.Monad.ST
+import Control.Monad
+import Data.Array.Unboxed
+
+euler23 = sum [x | (x, True) <- (assocs . obtainNonAbundantSums) $ abundantNos maxNo]
+
+maxNo = 28123
+
+obtainNonAbundantSums :: [Int] -> UArray Int Bool
+obtainNonAbundantSums abNos= runSTUArray $ do
+    arr <- newArray (1, maxNo) True
+    forM_ abNos $ \m -> do
+        let xs = takeWhile (\a -> m + a <= maxNo) $ dropWhile (< m) abNos
+        forM_ xs $ \n -> writeArray arr (m + n) False
+    return arr
+
+abundantNos n = filter (\n -> sumProperDivisors n > n) [1..n] 
+
+sumProperDivisors n 
+  | n == 1    = 0
+  | otherwise = sum factors - n
+    where factors = concatMap (\(x,y)-> if x /= y then [x,y] else [x]) $ factorPairs n
+     
+factorPairs :: Int -> [(Int, Int)]
+factorPairs x = [ (y, x `div` y) | y <- [1..truncate (sqrt (fromIntegral x))], x `mod` y == 0]
+
+main = putStrLn (show euler23)
